aeb84d35c18be0d316c59d32b7a0acf05f2666b1
Andrew Jenkins <andrewjjenkins@gmail.com>
2023-03-06 19:20:34 +0000

    Add github action to build docker container

--- ubpf/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-emcc -g -O0 -s MODULARIZE=1 -s ENVIRONMENT="web" -D__x86_64__=1 -I./inc -o ubpf.js ubpf_vm.c ubpf_jit_x86_64.c ebpfvm_emscripten.c
-sed -i '1 i\ /* eslint-disable */' ubpf.js
-cp ubpf.js ../src/vm/ubpf.js
-cp ubpf.wasm ../public/ubpf.wasm
-
