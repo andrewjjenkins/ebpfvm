8996ab9f265b09e5aecc24a811619e6666ecf1cf
Andrew Jenkins <andrewjjenkins@gmail.com>
2023-04-20 18:29:52 +0000

    Apply Prettier to ProgramEditor, no code change

--- src/ProgramEditor.tsx
+++ src/ProgramEditor.tsx
@@ -19,52 +19,52 @@ import CodeMirror from "@uiw/react-codemirror";
 import { Box, Typography } from "@mui/material";
 
 const errorBoxStyle = {
-  border: "2px solid grey",
-  borderRadius: "5px",
-  minHeight: "1lh",
-  padding: "4px",
-  display: "flex",
+    border: "2px solid grey",
+    borderRadius: "5px",
+    minHeight: "1lh",
+    padding: "4px",
+    display: "flex",
 };
 
 const printkTextStyle = {
-  fontFamily: "Monospace",
-  fontSize: "default",
-  margin: "1px",
-  whiteSpace: "pre-line",
+    fontFamily: "Monospace",
+    fontSize: "default",
+    margin: "1px",
+    whiteSpace: "pre-line",
 };
 
 interface LastErrorProps {
-  lastError: string | null;
+    lastError: string | null;
 }
 
 const LastError: FC<LastErrorProps> = (props) => {
-  if (!props.lastError || props.lastError === "") {
-    return null;
-  }
+    if (!props.lastError || props.lastError === "") {
+        return null;
+    }
 
-  return (
-    <Box>
-      <Typography variant="h6" component="div">
-        Assembler Error
-      </Typography>
-      <Box sx={errorBoxStyle}>
-        <Typography sx={printkTextStyle}>{props.lastError}</Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box>
+            <Typography variant="h6" component="div">
+                Assembler Error
+            </Typography>
+            <Box sx={errorBoxStyle}>
+                <Typography sx={printkTextStyle}>{props.lastError}</Typography>
+            </Box>
+        </Box>
+    );
 };
 
 interface ProgramEditProps {
-  source: string;
-  onChange: (newValue: string) => void;
-  lastError: string;
+    source: string;
+    onChange: (newValue: string) => void;
+    lastError: string;
 }
 
 export const ProgramEditView: FC<ProgramEditProps> = (props) => {
-  return (
-    <Box>
-      <CodeMirror value={props.source} onChange={props.onChange} />
-      <LastError lastError={props.lastError} />
-    </Box>
-  );
+    return (
+        <Box>
+            <CodeMirror value={props.source} onChange={props.onChange} />
+            <LastError lastError={props.lastError} />
+        </Box>
+    );
 };
